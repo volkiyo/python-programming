3bc2546abcdee5f7c23a1a5ffd2cee4e548e31cb
volkiyo <volkiyo@gmail.com>
2021-11-16 09:55:30 +0900

    python source

---
+++ mycode/first.py
@@ -0,0 +1,9 @@
+def add(n1,n2)
+    return n1+n2
+
+print(add(10,20))
+
+class User:
+    #생성자 선언
+    def __init__(self,name):
+        self.name = name
---
+++ mycode/lab/1fahrenheit.py
@@ -0,0 +1,11 @@
+"""
+섭씨온도를 입력 받아서 화씨로 변환하기
+"""
+
+# 섭씨를 화씨로 변환하는 함수
+def fah_convert(celsius):
+    return((9/5 * float(celsius)) +32
+
+print("변환하고 싶은 섭씨 온도를 입력하세요!")
+user_input = input()
+print(type(user_input), user_input)
